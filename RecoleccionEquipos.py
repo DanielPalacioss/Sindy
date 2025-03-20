cef59904f98de4f702680d24abbc283e58d4c5b6
Daniel <arindaniel17@gmail.com>
2025-03-19 19:39:48 -0500

    cambios mas recientes

--- RecoleccionEquipos.py
+++ RecoleccionEquipos.py
@@ -2,42 +2,71 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from bs4 import BeautifulSoup
 import time
+import pandas as pd
+import os
 
-# Configuración de Selenium con ChromeDriver
-service =  Service(executable_path= 'chromedriver.exe')
-driver = webdriver.Chrome(service=service)
+class EquipmentCollection:
+    if __name__ == "__main__":
+        # Configuración de Selenium con ChromeDriver
+        service =  Service(executable_path= 'chromedriver.exe')
+        driver = webdriver.Chrome(service=service)
 
-urls_torneos = []
+        urls_torneos = []
 
-equipos = {"Desconocido": -1}
+        equipos_dict = {"Desconocido": -1}
 
 
-def getTeams(soup):
-    try:
-        campeonato = soup.find('div', class_= 'PZPZlf ssJ7i B5dxMb')
-        team_table = soup.find('table', class_= 'Jzru1c')
-
-        rows = team_table.select('tr.imso-loa.imso-hov')
-        for row in rows:
-            team_name = row.get("aria-label")
-            if not team_name in equipos:
-                equipos[team_name] = (len(equipos)+1)-1
-
-    except Exception as e:
-        print(f"Error al obtener estadísticas para {campeonato}: {e}")
-
-# Función para procesar las URLs
-def procesar_urls(urls):
-    for url in urls:
+    def getTeams(self, soup):
+        self.equipos_dict = self.get_dict_of_csv()
         try:
-            driver.delete_all_cookies()
-            driver.get(url)
-            time.sleep(5)
-            soup = BeautifulSoup(driver.page_source, 'html.parser')
-            getTeams(soup)
-            
-        except Exception as e:
-            print(f"Error procesando la URL {url}: {e}")
+            campeonato = soup.find('div', class_='PZPZlf ssJ7i')
+            team_tables = soup.find_all('table', class_='Jzru1c')  # Encuentra todas las tablas
 
-procesar_urls(urls_torneos)
-print(equipos)
+            for team_table in team_tables:  # Itera sobre cada tabla encontrada
+                rows = team_table.select('tr.imso-loa.imso-hov')  # Selecciona las filas dentro de cada tabla
+                for row in rows:
+                    team_name = row.get("aria-label")
+                    if team_name and team_name not in self.equipos_dict:
+                        self.equipos_dict[team_name] = len(self.equipos_dict)
+
+        except Exception as e:
+            print(f"Error al obtener estadísticas para {campeonato}: {e}")
+
+    # Función para procesar las URLs
+    def procesar_urls(self, urls):
+        for url in urls:
+            try:
+                self.driver.delete_all_cookies()
+                self.driver.get(url)
+                time.sleep(3)
+                soup = BeautifulSoup(self.driver.page_source, 'html.parser')
+                self.getTeams(soup)
+                
+            except Exception as e:
+                print(f"Error procesando la URL {url}: {e}")
+
+        # Cerrar el navegador
+        self.driver.quit()
+
+        return pd.DataFrame(list(self.equipos_dict.items()), columns=["Equipo", "ID"])
+
+    def get_dict_of_csv(self):
+        if os.path.isfile('teams.csv'):
+            df = pd.read_csv('teams.csv', sep=';', quotechar='"')
+            return dict(zip(df["Equipo"], df["ID"]))
+        else:
+            return self.equipos_dict
+        
+    def save_csv(self, df):
+
+        # Verificar si el archivo existe
+        if os.path.isfile('teams.csv'):
+            df.to_csv('teams.csv', sep=';', index=False)
+        else:
+            df.to_csv('teams.csv', sep=';', index=False)
+            # Si no existe, se crea desde 0
+            
+if __name__ == "__main__":
+    equipment_collection = EquipmentCollection()
+    df = equipment_collection.procesar_urls(equipment_collection.urls_torneos)
+    equipment_collection.save_csv(df)
